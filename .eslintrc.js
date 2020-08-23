84d7ebeb0a0d15d0eaefaa62eee88ca8830667f4
Andrés Reverón Molina <andressser@gmail.com>
2020-08-23 11:58:36 +0200

    add eslint-plugin-only-warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   extends: ["plugin:vue/essential", "eslint:recommended"],
-  rules: {},
+  plugins: ["only-warn"],
   parserOptions: {
     parser: "babel-eslint"
   }
