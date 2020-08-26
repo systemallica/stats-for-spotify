340dd50fa3ddbb376244e12944e6cdde4be9e96d
Andrés Reverón Molina <andressser@gmail.com>
2020-08-26 12:12:40 +0200

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="container">
-    <h2>This is an about page</h2>
+    <h3>This page was built with love and <a href="https://vuejs.org/">Vue.js</a>.</h3>
+    <h3>The source code will be available shortly.</h3>
+    <h3>No data is collected.</h3>
   </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
 }
 </style>
