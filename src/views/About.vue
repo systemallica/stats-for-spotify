f4fe5cb1907a6562098a52da844dd2b389c7e862
Andrés Reverón Molina <andres@reveronmolina.me>
2022-01-07 21:08:12 +0100

    chore: add source code link

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container">
-    <h3>This page was built with love and <a href="https://vuejs.org/">Vue.js</a>.</h3>
-    <h3>The source code will be available shortly.</h3>
+    <h3>
+      This page was built with love and <a href="https://vuejs.org/">Vue.js</a>.
+    </h3>
+    <h3>
+      The source code is available
+      <a href="https://github.com/systemallica/stats-for-spotify/">here</a>.
+    </h3>
     <h3>No data is collected.</h3>
   </div>
 </template>
